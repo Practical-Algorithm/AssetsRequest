ee3bcc0b8b47e7a220506cffbbbaacd78b409b62
Patiphon Loetsuthakun <36232545+Phon1209@users.noreply.github.com>
2025-06-21 15:54:57 +0700

    Update from Laptop

--- bot_config.py
+++ bot_config.py
@@ -1,7 +1,7 @@
 import os
 import load_dotenv
 
-load_dotenv.load_dotenv() # Load environment variables from .env file
+load_dotenv.load_dotenv()  # Load environment variables from .env file
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_COMMAND_PREFIX = os.getenv('DISCORD_COMMAND_PREFIX')
 CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
@@ -10,4 +10,3 @@ DISCORD_ADMIN_ID = int(os.getenv('DISCORD_ADMIN_ID'))
 
 NOTION_TOKEN = os.getenv('NOTION_API_KEY')
 DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
-
